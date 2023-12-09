69446bd17dd4064ff8e99f4f5f726f1fc078afe3
zichenstudio@outlook.com <zichenstudio@outlook.com>
2023-12-10 00:56:08 +0800

    Update contents

---
+++ script/contents.js
@@ -0,0 +1,19 @@
+(function () {
+    let content = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+    console.log(content);
+    let result = "";
+    for (let i = 1; i < content.length; i++) {
+        result += `<a href="#${content[i].id}" onclick="smoothScroll(event)">
+            ${content[i].tagName
+                ?.replace("H1", "# ")
+                ?.replace("H2", "## ")
+                ?.replace("H3", "### ")
+                ?.replace("H4", "#### ")
+                ?.replace("H5", "##### ")
+                ?.replace("H6", "###### ")}
+            ${content[i].innerText}
+            <br>`;
+    }
+    console.log(result);
+    document.getElementById("content").innerHTML = result;
+}())
